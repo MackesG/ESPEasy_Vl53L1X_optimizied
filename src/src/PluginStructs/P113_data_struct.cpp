803937d6e4bc4b00e8d53e1309f394e531395584
MackesG <Mackes@gmx.de>
2023-10-20 06:20:34 +0200

    Update P113_data_struct.cpp

--- src/src/PluginStructs/P113_data_struct.cpp
+++ src/src/PluginStructs/P113_data_struct.cpp
@@ -39,7 +39,7 @@ bool P113_data_struct::begin() {
 
     if (range) {
       sensor->setDistanceModeLong();
-      sensor->setROI(4,4,199);
+      sensor->setROI(16,6,199);
     } else {
       sensor->setDistanceModeShort();
     }
